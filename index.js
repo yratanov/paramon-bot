b0dce9df16d6738be0d10fdd3a4f26b649ce873b
yratanov <organium@gmail.com>
2018-05-28 22:49:19 +0700

    remove pollution

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const Telegraf = require('telegraf');
 const token = process.env.TELEGRAM_TOKEN;
 
 const bot = new Telegraf(token);
-
 bot.telegram.getMe().then((botInfo) => {
   bot.options.username = botInfo.username
 });
 
 const dsMessage = require('./lib/dark_souls_message');
-const pollution = require('./lib/pollution');
 const exchange = require('./lib/exchange_rate');
 
 dsMessage(bot);
-pollution(bot);
 exchange(bot);
 bot.startPolling();
